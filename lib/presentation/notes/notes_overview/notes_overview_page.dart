24652e2a8358d9a407a4af435f8a362ba3e48916
Lahib Youssef <lmy@tat.dev>
2021-02-25 13:20:20 +0100

    wip

--- lib/presentation/notes/notes_overview/notes_overview_page.dart
+++ lib/presentation/notes/notes_overview/notes_overview_page.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_firebase_ddd_course/application/auth/auth_bloc.dart';
 
 class NotesOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+        leading: IconButton(
+          icon: const Icon(Icons.exit_to_app),
+          onPressed: () {
+            context.read<AuthBloc>().add(const AuthEvent.signedOut());
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.indeterminate_check_box),
+            onPressed: () {},
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // TODO: Navigate to NoteFormPage
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
